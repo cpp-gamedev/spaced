c5d13e2c32f2a3378c60d873535f976204a8a2f3
Karn Kaul <karnkaul@gmail.com>
2024-03-04 22:05:39 +0530

    Show two decimal points of health.

--- src/spaced/spaced/game/health.cpp
+++ src/spaced/spaced/game/health.cpp
@@ -19,7 +19,7 @@ void Health::do_inspect() {
 			bave::im_text("health: {:.2f}", m_hit_points);
 			bave::im_text("dead: {}", is_dead() ? "yes" : "no");
 			if (ImGui::Button("inflict 10 damage")) { inflict_damage(10.f); }
-			ImGui::SliderFloat("health", &m_hit_points, 0.f, 100.f, "%.1f");
+			ImGui::SliderFloat("health", &m_hit_points, 0.f, 100.f, "%.2f");
 			ImGui::TreePop();
 		}
 	}
