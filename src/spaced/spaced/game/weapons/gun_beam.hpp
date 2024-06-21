d853b411494e66515268e8c53d35d72ec752de61
Karn Kaul <karnkaul@gmail.com>
2024-06-21 13:59:08 +0530

    Use gradient texture for beam.

--- src/spaced/spaced/game/weapons/gun_beam.hpp
+++ src/spaced/spaced/game/weapons/gun_beam.hpp
@@ -8,14 +8,14 @@ class GunBeam final : public Weapon {
 		float beam_height{5.0f};
 		bave::Seconds fire_duration{2s};
 		bave::Seconds reload_delay{1s};
-		bave::Rgba beam_tint{bave::red_v};
+		std::shared_ptr<bave::Texture const> beam_texture{};
 		float dps{10.0f};
 	};
 
 	explicit GunBeam(bave::Services const& services);
 
 	[[nodiscard]] auto is_idle() const -> bool final { return m_fire_remain <= 0s; }
-	[[nodiscard]] auto get_icon() const -> std::shared_ptr<bave::Texture const> final { return m_icon; }
+	[[nodiscard]] auto get_icon() const -> std::shared_ptr<bave::Texture const> final { return config.beam_texture; }
 
 	void tick(bave::Seconds dt) final;
 
@@ -24,8 +24,6 @@ class GunBeam final : public Weapon {
   private:
 	auto do_fire(glm::vec2 muzzle_position) -> std::unique_ptr<Round> final;
 	void do_inspect() final;
-
-	std::shared_ptr<bave::Texture const> m_icon{};
 
 	bave::Seconds m_fire_remain{};
 	bave::Seconds m_reload_remain{};
